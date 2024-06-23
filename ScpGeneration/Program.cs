e61f2caa2b0b62e6d59be1e3f79e42e12b025702
Wellsaik <artem.podorozhko@outlook.com>
2024-06-23 11:16:24 +0300

    Empty vs solution

---
+++ ScpGeneration/Program.cs
@@ -0,0 +1,15 @@
+﻿using Edgar.Geometry;
+using Edgar.GraphBasedGenerator.Grid2D.Drawing;
+using Edgar.GraphBasedGenerator.Grid2D;
+using Edgar.GraphBasedGenerator.Common;
+using System.Diagnostics;
+using Edgar.Graphs;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+    }
+}
